a0ddaeb2bf5ed69eb2151e0f5377a630f4e2c5af
Philippe Rathé <prathe@gmail.com>
2018-10-18 10:18:42 -0400

    Implement state and dynamic data flow

--- src/components/CityLocator/CityLocator.js
+++ src/components/CityLocator/CityLocator.js
@@ -4,10 +4,28 @@ import CityLocatorTable from './CityLocatorTable';
 import CityLocatorMap from './CityLocatorMap';
 
 class CityLocator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ''
+    };
+
+    this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+  }
+
+  handleSearchTextChange(searchText) {
+    this.setState({
+      searchText: searchText
+    });
+  }
+
   render() {
     return (
       <div>
-        <CityLocatorSearchBar />
+        <CityLocatorSearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.handleSearchTextChange}
+        />
         <CityLocatorTable cities={this.props.cities} />
         <CityLocatorMap />
       </div>
--- src/components/CityLocator/CityLocatorSearchBar.js
+++ src/components/CityLocator/CityLocatorSearchBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class CityLocatorSearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+  }
+  
+  handleSearchTextChange(e) {
+    this.props.onSearchTextChange(e.target.value);
+  }
+
   render() {
+    const searchText = this.props.searchText;
+
     return (
       <form>
-        <input type="text" placeholder="Search city..." />
+        <input
+          type="text"
+          placeholder="Search city..."
+          value={searchText}
+          onChange={this.handleSearchTextChange} />
       </form>
     );
   }
