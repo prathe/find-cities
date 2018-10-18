49bd82c65e05bbab397fd36dc6c869b71023938a
Philippe Rathé <prathe@gmail.com>
2018-10-18 13:46:34 -0400

    Add no results section

--- src/components/CityLocator/CityLocatorTable.js
+++ src/components/CityLocator/CityLocatorTable.js
@@ -3,21 +3,31 @@ import CityLocatorTableHeader from './CityLocatorTableHeader';
 import CityLocatorTableRow from './CityLocatorTableRow';
 
 class CityLocatorTable extends React.Component {
-  render() {
+  render() {  
     const city_locator_table_rows = [];
 
     this.props.cities.forEach((city) => {
       city_locator_table_rows.push(<CityLocatorTableRow city={city} />);
     });
 
+    const no_result = (
+      <p>If we found a match, it will appear right here...</p>
+    )
+
+    const table = (
+      <table>
+        <CityLocatorTableHeader />
+        <tbody>
+          {city_locator_table_rows}
+        </tbody>
+      </table>
+    );
+
+    const widget = city_locator_table_rows.length === 0 ? no_result : table
+
     return (
       <div>
-        <table>
-          <CityLocatorTableHeader />
-          <tbody>
-            {city_locator_table_rows}
-          </tbody>
-        </table>
+        {widget}
       </div>
     );
   }
