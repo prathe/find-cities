e1e35209118605f7e68164401761387630794e7a
Philippe Rathé <prathe@gmail.com>
2018-10-20 19:45:18 -0400

    trying Google map

--- src/components/CityLocator/CityLocatorMap.js
+++ src/components/CityLocator/CityLocatorMap.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
 
-class CityLocatorMap extends React.Component {
-  render() {
-    return (
-      <p className="p-3">Map would go here</p>
-    )
+function CityLocatorMap(props) {
+
+  const defaultCenter = {lat: 46.81228, lng: -71.21454} // Quebec, CA
+  let center
+  if (props.cities.length > 0) {
+    const city = props.cities[0]
+    center = {lat: city.lat, lng: city.lng}
   }
+
+  console.log(defaultCenter)
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        defaultZoom={3}
+        defaultCenter={defaultCenter}
+        center={center}
+        //options={MAP.options}
+        //onChange={this.handleMapChange}
+        yesIWantToUseGoogleMapApiInternals
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API }}
+      />
+    </div>
+  )
 }
 
 export default CityLocatorMap
